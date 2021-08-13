c72a3514b9d425582245f5453fccd3255da6a8ec
Etienne Casanova <etmaca@gmail.com>
2021-08-13 14:25:32 -0400

    missing file

---
+++ TakeoffWiz/Aircraft.swift
@@ -0,0 +1,19 @@
+//
+//  Aircraft.swift
+//  TakeoffWiz
+//
+//  Created by Etienne Casanova on 8/13/21.
+//
+
+import Foundation
+
+protocol Aircraft {
+//    func giveMeTheAircraftName() -> String
+
+    func takeoffRollGenericCalc()
+    func takeoffRoll50ftCalc()
+    func gradientCalc()
+    func climbRateCalc()
+    func executeCalc()
+    
+}
